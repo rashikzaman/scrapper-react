bacb5db045b7bf31d7f305579a50f95c7d45e7e0
Rashik Zaman <rashikzaman13@gmail.com>
2022-01-19 22:37:46 +0600

    Added html file link

--- components/Dashboard/Keywords.js
+++ components/Dashboard/Keywords.js
@@ -8,7 +8,8 @@ import TableRow from '@mui/material/TableRow';
 import Title from './Title';
 
 
-export default function Keywords({keywords}) {
+export default function Keywords({ keywords }) {
+
 
   return (
     <React.Fragment>
@@ -20,7 +21,8 @@ export default function Keywords({keywords}) {
             <TableCell>Status</TableCell>
             <TableCell>Search Result</TableCell>
             <TableCell>Adword</TableCell>
-            <TableCell>Link</TableCell>
+            <TableCell>Total Link</TableCell>
+            <TableCell>HTML FIle</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -31,10 +33,17 @@ export default function Keywords({keywords}) {
               <TableCell>{row.search_result}</TableCell>
               <TableCell>{row.adword}</TableCell>
               <TableCell>{row.link}</TableCell>
+              <TableCell><a href={getHtmlLink(row.html_file_path)} target="_blank" rel="noreferrer">LINK</a></TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
     </React.Fragment>
   );
+}
+
+
+const getHtmlLink = (link) => {
+  const htmlLink = process.env.NEXT_PUBLIC_SERVER_STATIC_HOST + link
+  return htmlLink
 }
